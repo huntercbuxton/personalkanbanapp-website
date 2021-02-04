26446baff676ac22b4815454ef6a339878e6f1db
Hunter Buxton <hunter.buxton@protonmail.com>
2021-02-04 09:00:31 -0700

    updates to the home page

--- index.php
+++ index.php
@@ -1,35 +1,49 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset='utf-8'>
 <title>Personal Kanban App</title>
 
-<link rel="stylesheet" href="styles.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name='author' content='Hunter Buxton'>
+<meta name='description' content='Personal Kanban app support'>
+<meta name='keywords' content='PersonalKanban, Kanban, App, Agile, Support, Hunter, Buxton'>
+
+<link rel="stylesheet" href="/css/style.css">
 <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
 <link rel="manifest" href="/manifest.webmanifest">
+<!-- https://fonts.google.com/specimen/Antic+Slab?preview.text_type=custom&sidebar.open=true&selection.family=Antic+Slab -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Antic+Slab&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
-
+<div id='spacer'>
 <header>
 <img src="/img/apple-touch-icon.png">
 <h1>The Personal Kanban App</h1>
 </header> 
 
+<main>
 <section>
-    <p>PersonalKanban is an iOS app which adapts the Kanban Method to your personal workflows. I designed this app as a personal development project to teach myself some iOS coding skills, and I hope you enjoy using it as much as I enjoyed making it. The app is currently on its first release, and I plan on releasing a some new features soon. For a list of upcoming features, please refer to the github README (link below).</p>
+    <p>PersonalKanban is an iOS app which adapts the Kanban Method to your personal workflows. I designed this app as a personal development project to teach myself some iOS coding skills, and I hope you enjoy using it as much as I enjoyed making it. The latest release of this app is version 2.1; I plan on releasing a some new features soon in version 2.2. You can find a list of upcoming features in the <a href='https://github.com/huntercbuxton/PersonalKanbanApp/blob/main/README.md'>README.md</a> on GitHub.</p>
 </section>
 
 <section>
     <h2>A few important details:</h2>
 
-    <p>PersonalKanban does not collect any data remotely, and does not store any data on your device besides the info you input and save yourself. You may click <a href="PrivacyPolicy.php">here</a> to view the privacy policy.</p>
+    <p>PersonalKanban does not collect any data remotely, and does not store any data on your device besides the info you input and save yourself. You may click <a href="/PrivacyPolicy.php">here</a> to view the privacy policy.</p>
     <p>You can view additional info including the source code on the public github repo <a href="https://github.com/huntercbuxton/PersonalKanbanApp">here</a>.</p>
-    <p>For additional support or input you can contact the developer (me) at <a href = "mailto: huntercbuxton@icloud.com">huntercbuxton@icloud.com</a>.  I typically respond within 1-2 business days.</p>
+    <p>For additional support or input you can contact the developer (me) at <a href = "mailto: huntercbuxton@icloud.com">huntercbuxton@icloud.com</a>.  I typically respond within 1-2 business days. You can also find info on my other projects on my personal website, <a href='http://hunterbuxton.com'>hunterbuxton.com</a></p>
     <p>Thanks! I hope you enjoy this app.</p>
     <p> &nbsp; &nbsp; &nbsp; -Hunter Buxton</p>
 </section>
+</main>
 
+<footer>&copy; 2021 Hunter Buxton. All rights reserved.</footer>
+</div>
 </body>
 </html>
